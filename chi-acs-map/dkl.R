303638f8845ac134349111d4776b4575f8665380
Nicholas Marchio <namarchio@gmail.com>
2021-03-24 23:44:16 -0500

    Update dkl.R

--- chi-acs-map/dkl.R
+++ chi-acs-map/dkl.R
@@ -1,5 +1,4 @@
 
-
 library(tidyverse)
 library(sf)
 library(lwgeom)
@@ -76,8 +75,19 @@ acs5_vars <- acs5_vars %>% separate(col = 'concept',
 # Create HTML Search Window to find variables
 explore_acs_vars()
 
-acs5_vars_selected <- c('B06010_002','B06010_004','B06010_005','B06010_006','B06010_007','B06010_008','B06010_009','B06010_010','B06010_011',
-                        'B25121_002','B25121_017','B25121_032','B25121_047','B25121_062','B25121_077','B25121_092',
+acs5_vars_selected <- c('B23025_004','B23025_005','B23025_006','B23025_007',
+                        'B06001_002','B06001_003','B06001_004','B06001_005','B06001_006','B06001_007','B06001_008','B06001_009','B06001_010','B06001_011','B06001_012',
+                        'B19001_002','B19001_003','B19001_004','B19001_005','B19001_006','B19001_007','B19001_008','B19001_009','B19001_010','B19001_011','B19001_012','B19001_013','B19001_014','B19001_015','B19001_016','B19001_017',
+                        'B19001A_002','B19001A_003','B19001A_004','B19001A_005','B19001A_006','B19001A_007','B19001A_008','B19001A_009','B19001A_010','B19001A_011','B19001A_012','B19001A_013','B19001A_014','B19001A_015','B19001A_016','B19001A_017',
+                        'B19001B_002','B19001B_003','B19001B_004','B19001B_005','B19001B_006','B19001B_007','B19001B_008','B19001B_009','B19001B_010','B19001B_011','B19001B_012','B19001B_013','B19001B_014','B19001B_015','B19001B_016','B19001B_017',
+                        'B19001C_002','B19001C_003','B19001C_004','B19001C_005','B19001C_006','B19001C_007','B19001C_008','B19001C_009','B19001C_010','B19001C_011','B19001C_012','B19001C_013','B19001C_014','B19001C_015','B19001C_016','B19001C_017',
+                        'B19001C_002','B19001C_003','B19001C_004','B19001C_005','B19001C_006','B19001C_007','B19001C_008','B19001C_009','B19001C_010','B19001C_011','B19001C_012','B19001C_013','B19001C_014','B19001C_015','B19001C_016','B19001C_017',
+                        'B19001D_002','B19001D_003','B19001D_004','B19001D_005','B19001D_006','B19001D_007','B19001D_008','B19001D_009','B19001D_010','B19001D_011','B19001D_012','B19001D_013','B19001D_014','B19001D_015','B19001D_016','B19001D_017',
+                        'B19001E_002','B19001E_003','B19001E_004','B19001E_005','B19001E_006','B19001E_007','B19001E_008','B19001E_009','B19001E_010','B19001E_011','B19001E_012','B19001E_013','B19001E_014','B19001E_015','B19001E_016','B19001E_017',
+                        'B19001F_002','B19001F_003','B19001F_004','B19001F_005','B19001F_006','B19001F_007','B19001F_008','B19001F_009','B19001F_010','B19001F_011','B19001F_012','B19001F_013','B19001F_014','B19001F_015','B19001F_016','B19001F_017',
+                        'B19001G_002','B19001G_003','B19001G_004','B19001G_005','B19001G_006','B19001G_007','B19001G_008','B19001G_009','B19001G_010','B19001G_011','B19001G_012','B19001G_013','B19001G_014','B19001G_015','B19001G_016','B19001G_017',
+                        'B19001H_002','B19001H_003','B19001H_004','B19001H_005','B19001H_006','B19001H_007','B19001H_008','B19001H_009','B19001H_010','B19001H_011','B19001H_012','B19001H_013','B19001H_014','B19001H_015','B19001H_016','B19001H_017',
+                        'B19001I_002','B19001I_003','B19001I_004','B19001I_005','B19001I_006','B19001I_007','B19001I_008','B19001I_009','B19001I_010','B19001I_011','B19001I_012','B19001I_013','B19001I_014','B19001I_015','B19001I_016','B19001I_017',
                         'B08126_002','B08126_003','B08126_004','B08126_005','B08126_006','B08126_007','B08126_008','B08126_009','B08126_010','B08126_011','B08126_012','B08126_013','B08126_014','B08126_015',        
                         'B08124_002','B08124_003','B08124_004','B08124_005','B08124_006','B08124_007',
                         'B15003_002','B15003_003','B15003_004','B15003_005','B15003_006','B15003_007','B15003_008','B15003_009','B15003_010','B15003_011','B15003_012','B15003_013','B15003_014','B15003_015','B15003_016',
@@ -107,12 +117,16 @@ acs_tract_all_us_data <- map_df(state_list, function(x) {
           state = x)
 })
 
+explore_acs_vars()
 options(scipen=999)
+
 df <- acs_tract_all_us_data %>%
   rename_all(list(tolower)) %>%
   left_join(., acs5_vars %>% select(name, label), by = c('variable' = 'name')) %>%
   mutate(label = gsub("Estimate!!Total:", "", label), 
          label = gsub("With income:", "", label),
+         label = gsub("In labor force", "", label),
+         label = gsub("Civilian labor force", "", label),
          label = gsub("Household income the past 12 months \\(in 2019 inflation-adjusted dollars\\) --", "", label),
          label = gsub("", "", label),
          label = gsub("!!|:", "", label)) %>%
@@ -121,13 +135,33 @@ df <- acs_tract_all_us_data %>%
            sep = c('_'),
            remove = FALSE,
            extra = "merge") %>%
+  mutate(label = case_when(variable_group == 'B19001A' ~ paste0('White alone',' - ',label),
+                           variable_group == 'B19001B' ~ paste0('Black or African American alone',' - ',label),
+                           variable_group == 'B19001C' ~ paste0('Native American alone',' - ',label),
+                           variable_group == 'B19001D' ~ paste0('Asian alone',' - ',label),
+                           variable_group == 'B19001E' ~ paste0('Pacific Islander alone',' - ',label),
+                           variable_group == 'B19001F' ~ paste0('Some other race alone',' - ',label),
+                           variable_group == 'B19001G' ~ paste0('Two or more races',' - ',label),
+                           variable_group == 'B19001H' ~ paste0('White alone, not Latino',' - ',label),
+                           variable_group == 'B19001I' ~ paste0('Hispanic or Latino',' - ',label),
+                           TRUE ~ as.character(label))) %>%
   mutate(group_label = case_when(variable_group == "B02001" ~ 'Race',
-                                 variable_group == "B03001" ~ 'Latino',
-                                 variable_group == "B06010" ~ 'Individual Income',
+                                 variable_group == "B03001" ~ 'Hispanic or Latino',
                                  variable_group == "B08124" ~ 'Occupation',
                                  variable_group == "B08126" ~ 'Industry',
-                                 variable_group == "B15003" ~ 'Education',
-                                 variable_group == "B25121" ~ 'Household Income'),
+                                 variable_group == "B15003" ~ 'Education attainment',
+                                 variable_group == 'B23025' ~ 'Employment status',
+                                 variable_group == 'B06001' ~ 'Age',
+                                 variable_group == 'B19001' ~ 'Household income',
+                                 variable_group == 'B19001A' ~ 'Household income - White alone',
+                                 variable_group == 'B19001B' ~ 'Household income - Black or African American alone',
+                                 variable_group == 'B19001C' ~ 'Household income - Native American alone',
+                                 variable_group == 'B19001D' ~ 'Household income - Asian alone',
+                                 variable_group == 'B19001E' ~ 'Household income - Pacific Islander alone',
+                                 variable_group == 'B19001F' ~ 'Household income - Some other race alone',
+                                 variable_group == 'B19001G' ~ 'Household income - Two or more races',
+                                 variable_group == 'B19001H' ~ 'Household income - White alone, not Latino',
+                                 variable_group == 'B19001I' ~ 'Household income - Hispanic or Latino'),
          county_fips = str_sub(geoid,1,5)) %>%
   left_join(., state_xwalk, by = c('county_fips'='county_fips')) %>%
   left_join(.,cbsa_xwalk, by = c('county_fips'='county_fips')) %>%
@@ -142,12 +176,15 @@ df <- acs_tract_all_us_data %>%
   rename(tract_estimate = estimate,
          tract_fips = geoid) %>%
   select(tract_fips,county_fips,county_name,cbsa_fips,cbsa_title,area_type,central_outlying_county,state_codes,state_fips,state_name,variable,variable_group,variable_item,group_label,label,tract_pct,tract_estimate,moe,tract_total,county_pct,county_estimate,county_total,cbsa_pct,cbsa_estimate,cbsa_total) %>%
+  arrange(county_fips, tract_fips, variable) %>%
+  mutate_at(vars(tract_pct,county_pct,cbsa_pct), ~replace(., is.nan(.), 0)) %>%
   filter(cbsa_title %in% c("Chicago-Naperville-Elgin, IL-IN-WI",
-                           "Champaign-Urbana, IL",
                            "Madison, WI",
                            "Minneapolis-St. Paul-Bloomington, MN-WI",
                            "Indianapolis-Carmel-Anderson, IN",
                            "Milwaukee-Waukesha, WI"))
-  
+
 write_csv(df, '/Users/nm/Desktop/dkl_input_data.csv')
+
+write_csv(df %>% filter(tract_fips == '17031010100'), '/Users/nm/Desktop/dkl_test.csv')
 
